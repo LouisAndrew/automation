6417ee2946c0aa8017486a618c5594a913e6c61f
Louis Andrew <louis.andrew@share-now.com>
2021-07-08 18:08:55 +0200

    feat(next-due): get notion pages that has the next due value of today (1)

---
+++ DbType.ts
@@ -0,0 +1,4 @@
+export const DbType = {
+  INTERVALL: { name: "Intervall (Days)", val: "number" },
+  NEXT_DUE: { name: "Next Due", val: "date" },
+} as const;
--- handler.ts
+++ handler.ts
@@ -1,8 +1,34 @@
-import { v4 as uuid } from "uuid";
-
+import { Client } from "@notionhq/client";
+import { DbType } from "./DbType";
+// get recurring tasks that has the "Next Due" property of today.
 export const run = async (event, context) => {
-  const time = new Date();
-  console.log(`Your cron function "${context.functionName}" ran at ${time}`);
-  console.log(`uuid is: ${uuid()}`);
-  console.log("Is now using TS!");
+  const notion = new Client({
+    auth: process.env.NOTION_API_KEY,
+  });
+
+  const dbId = process.env.NOTION_DB_ID || "";
+
+  const todayDate = new Date().toISOString();
+  const response = await notion.databases.query({
+    database_id: dbId,
+    filter: {
+      and: [
+        {
+          property: DbType.INTERVALL.name,
+          [DbType.INTERVALL.val]: {
+            greater_than: 0,
+          },
+        },
+        {
+          property: DbType.NEXT_DUE.name,
+          [DbType.NEXT_DUE.val]: {
+            is_not_empty: true,
+            equals: todayDate,
+          },
+        },
+      ],
+    },
+  });
+
+  console.log(response);
 };
