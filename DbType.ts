66346449f15fb341eafc678cb67d1c890877f5fc
Louis Andrew <louis.andrew@share-now.com>
2021-07-09 15:03:49 +0200

    feat(handler): Done with daily intervall automation (2)

--- DbType.ts
+++ DbType.ts
@@ -1,4 +1,11 @@
 export const DbType = {
   INTERVALL: { name: "Intervall (Days)", val: "number" },
   NEXT_DUE: { name: "Next Due", val: "date" },
+  STATE: {
+    name: "State",
+    val: "select",
+    selectValue: "🏁 SELECTED",
+    selectId: "{Qrh",
+    selectColor: "pink",
+  },
 } as const;
--- handler.ts
+++ handler.ts
@@ -1,4 +1,5 @@
 import { Client } from "@notionhq/client";
+import { SelectOption } from "@notionhq/client/build/src/api-types";
 import { DbType } from "./DbType";
 // get recurring tasks that has the "Next Due" property of today.
 export const run = async (event, context) => {
@@ -9,7 +10,7 @@ export const run = async (event, context) => {
   const dbId = process.env.NOTION_DB_ID || "";
 
   const todayDate = new Date().toISOString();
-  const response = await notion.databases.query({
+  const scheduled = await notion.databases.query({
     database_id: dbId,
     filter: {
       and: [
@@ -26,9 +27,35 @@ export const run = async (event, context) => {
             equals: todayDate,
           },
         },
+        // {
+        //   property: DbType.STATE.name,
+        //   [DbType.STATE.val]: {
+        //     equals: DbType.STATE.selectValue,
+        //   },
+        // },
       ],
     },
   });
 
-  console.log(response);
+  await Promise.all(
+    scheduled.results.map(async (page) => {
+      return notion.pages.update({
+        page_id: page.id,
+        properties: {
+          Due: {
+            type: "date",
+            date: {
+              start: todayDate,
+            },
+          },
+          [DbType.STATE.name]: {
+            type: DbType.STATE.val,
+            [DbType.STATE.val]: {
+              name: DbType.STATE.selectValue,
+            } as SelectOption,
+          },
+        },
+      });
+    })
+  );
 };
