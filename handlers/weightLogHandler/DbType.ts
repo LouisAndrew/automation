6e5b4c0dbb44ab0c3876eb6b3a4c5548099196ac
Louis Andrew <louis.andrew@share-now.com>
2021-07-11 23:44:42 +0200

    feat(weight-autom): done with weight log automation (5)

--- handlers/weightLogHandler/DbType.ts
+++ handlers/weightLogHandler/DbType.ts
@@ -4,4 +4,15 @@ export const DbType = {
     type: "formula",
     nestedType: "date",
   },
+  START_DATE: {
+    name: "Start Date",
+    nestedType: "created_time",
+    type: "date",
+  },
 } as const;
+
+const createPageNumbers = () => {
+  return new Array(7).map((_, i) => ({ [`#${i}`]: { number: 0 } }));
+};
+
+export const PageNumbers = createPageNumbers();
--- handlers/weightLogHandler/index.ts
+++ handlers/weightLogHandler/index.ts
@@ -1,8 +1,57 @@
 import { Client } from "@notionhq/client";
+import {
+  PagesCreateParameters,
+  PagesCreateResponse,
+} from "@notionhq/client/build/src/api-endpoints";
+import { Page } from "@notionhq/client/build/src/api-types";
 import { APIGatewayEvent, Callback, Context } from "aws-lambda";
-import { DbType } from "./DbType";
+import { DbType, PageNumbers } from "./DbType";
 
-const getBetween = () => {};
+/**
+ * Get Date object of a start page
+ * @param page page to be extracted
+ */
+const getStartDate = (page: Page): Date =>
+  new Date(
+    page.properties[DbType.START_DATE.name][DbType.START_DATE.nestedType]
+  );
+
+/**
+ * Create new weekly entry on `Weight Logs` Notion DB
+ * @param notion notion client instance
+ * @param todayDate today's date in ISO string
+ */
+const createWeeeklyEntry = (
+  notion: Client,
+  todayDate: string,
+  dbId: string
+): Promise<PagesCreateResponse> => {
+  const date = new Date(todayDate);
+  const pad = (str: number) => (str < 10 ? `0${str}` : str);
+  return notion.pages.create({
+    parent: {
+      database_id: dbId,
+    },
+    properties: {
+      ...PageNumbers,
+      Name: {
+        title: [
+          {
+            text: {
+              content: `${pad(date.getDay() + 1)}.${pad(
+                date.getMonth() + 1
+              )}.${date.getFullYear()}`,
+            },
+          },
+        ],
+      },
+    },
+  } as unknown as PagesCreateParameters);
+};
+
+const getCurrentSlot = (page: Page, todayDate: string): number => {
+  return new Date(todayDate).getDate() - getStartDate(page).getDate() + 1;
+};
 
 // AWSLambda
 export const run = async (event: APIGatewayEvent, context: Context) => {
@@ -11,26 +60,47 @@ export const run = async (event: APIGatewayEvent, context: Context) => {
   });
 
   const dbId = process.env.NOTION_DB_ID || "";
+  const todayDate = new Date().toISOString();
 
   const weight = (event.body as any).weight;
-  console.log(typeof weight);
 
-  const todayDate = new Date().toISOString();
-  const entries = await notion.databases.query({
+  // get entry that for the current week
+  const { results: currentWeekEntry } = await notion.databases.query({
     database_id: dbId,
+    filter: {
+      and: [
+        {
+          property: DbType.START_DATE.name,
+          [DbType.START_DATE.type]: {
+            on_or_before: todayDate,
+          },
+        },
+        {
+          property: DbType.END_DATE.name,
+          [DbType.END_DATE.nestedType]: {
+            on_or_after: todayDate,
+          },
+        },
+      ],
+    },
   });
 
-  console.log(
-    await entries.results.map((r) => ({
-      properties: r.properties,
-      endDate: r.properties[DbType.END_DATE.name][DbType.END_DATE.type],
-    }))
-  );
+  const entry =
+    currentWeekEntry[0] || (await createWeeeklyEntry(notion, todayDate, dbId));
+  const currentSlot = getCurrentSlot(entry, todayDate);
+
+  await notion.pages.update({
+    page_id: entry.id,
+    properties: {
+      [`#${currentSlot}`]: {
+        type: "number",
+        number: weight as number,
+      },
+    },
+  });
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello, world!",
-    }),
+    body: null,
   };
 };
